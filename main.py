72611c9bc0e0011f054a3996ad92dfe7c8a69f9e
jonathan pinon <jonathanjosephpinon@gmail.com>
2025-02-06 15:22:17 -0800

    fps clock added

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    FPS_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = FPS_clock.tick(60)/1000
 
 
 if __name__ == "__main__":
