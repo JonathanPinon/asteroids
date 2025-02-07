5d47b37c731339de54a35efc37c2329b5ed3286a
jonathan pinon <jonathanjosephpinon@gmail.com>
2025-02-06 16:44:23 -0800

    player rendered

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -10,12 +8,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     FPS_clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = FPS_clock.tick(60)/1000
